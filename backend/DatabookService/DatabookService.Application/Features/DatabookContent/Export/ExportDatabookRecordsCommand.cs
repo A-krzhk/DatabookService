40971548bc01809d54950fa4c26dadabbed6eb64
Alena <alena-koryazhkina@mail.ru>
2025-11-08 22:50:41 +0300

    Fixed the record import method.

--- backend/DatabookService/DatabookService.Application/Features/DatabookContent/Export/ExportDatabookRecordsCommand.cs
+++ backend/DatabookService/DatabookService.Application/Features/DatabookContent/Export/ExportDatabookRecordsCommand.cs
@@ -1,7 +1,6 @@
 using DatabookService.Application.Interfaces;
 using DatabookService.Application.Interfaces.Services;
 using DatabookService.Domain.Entities;
-using DatabookService.Domain.Enums;
 using Microsoft.Extensions.Logging;
 using System.Globalization;
 using System.Text;
